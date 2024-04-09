500acf54722a5b34865a8e9cd4815b8207ae0348
Youngseok Hwang <hysgold@etechsystem.co.kr>
2024-04-09 09:11:38 +0900

    drawer update

--- src/components/AppBarAndNavigationDrawer.vue
+++ src/components/AppBarAndNavigationDrawer.vue
@@ -1,4 +1,5 @@
 <template>
+
   <v-app-bar
     color="teal-darken-4"
     image="https://picsum.photos/1920/1080?random"
@@ -10,19 +11,23 @@
     </template>
 
     <template v-slot:prepend>
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
     </template>
 
-    <v-app-bar-title>Title</v-app-bar-title>
+    <v-app-bar-title>Ring-On Service</v-app-bar-title>
 
     <v-spacer></v-spacer>
 
     <v-btn icon>
-      <v-icon>mdi-magnify</v-icon>
+      <v-icon>mdi-account-circle</v-icon>
     </v-btn>
 
     <v-btn icon>
-      <v-icon>mdi-heart</v-icon>
+      <v-icon>mdi-bell</v-icon>
+    </v-btn>
+
+    <v-btn icon>
+      <v-icon>mdi-cog</v-icon>
     </v-btn>
 
     <v-btn icon>
@@ -30,10 +35,31 @@
     </v-btn>
   </v-app-bar>
 
-  <v-navigation-drawer>
+  <v-navigation-drawer v-model="drawer"
+                        location="left"
+                        temporary
+  >
     <v-list>
-      <v-list-item title="Navigation drawer"></v-list-item>
+      <v-list-item title="Customer View"></v-list-item>
+    </v-list>
+    <hr>
+    <v-list nav>
+      <v-list-item prepend-icon="mdi-office-building" title="Platform" value="platform"></v-list-item>
+      <v-list-item prepend-icon="mdi-home" title="Dashboard" value="dashboard"></v-list-item>
+      <v-list-item prepend-icon="mdi-book-open-page-variant" title="My Page" value="myPage"></v-list-item>
+      <v-list-item prepend-icon="mdi-phone" title="Call List" value="callList"></v-list-item>
+      <v-list-item prepend-icon="mdi-account-group" title="User Manangement" value="userManagement"></v-list-item>
     </v-list>
   </v-navigation-drawer>
 
 </template>
+
+<script>
+  export default {
+    data () {
+      return {
+        drawer: null,
+      }
+    },
+  }
+</script>
