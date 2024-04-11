bd02ca814085acd58d3665972c60877825c518df
Youngseok Hwang <hysgold@etechsystem.co.kr>
2024-04-11 15:20:32 +0900

    commit

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,11 @@
+<template>
+  <p>footer</p>
+</template>
+
+<script setup>
+  
+</script>
+
+<style scoped lang="sass">
+  
+</style>
---
+++ src/components/NavigationDrawer.vue
@@ -0,0 +1,44 @@
+<template>
+    <v-navigation-drawer
+        v-model="drawer"
+        location="bottom"
+        temporary
+      >
+      <v-list
+        :items="items"
+      ></v-list>
+    </v-navigation-drawer>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      drawer: false,
+      group: null,
+      items: [
+        {
+          title: 'Foo',
+          value: 'foo',
+        },
+        {
+          title: 'Bar',
+          value: 'bar',
+        },
+        {
+          title: 'Fizz',
+          value: 'fizz',
+        },
+        {
+          title: 'Buzz',
+          value: 'buzz',
+        },
+      ],
+    }),
+
+    watch: {
+      group () {
+        this.drawer = false
+      },
+    },
+  }
+</script>
