43e5245a1196227514261a74c9503fcfed7ebab0
Youngseok Hwang <hysgold@etechsystem.co.kr>
2024-05-03 16:38:48 +0900

    Main page use fetched data

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -2,36 +2,35 @@
   <v-main class="d-flex align-center justify-center" style="min-height: 1000px;">
     <div>
       <h1>Main Page</h1>
-      <ul>
-        <li v-for="item in items" :key="item.id">{{ item }}</li>
-      </ul>
+      <!-- <p v-for="(item, index) in items">{{ items[index] }}</p> -->
+      <p v-for="item in items">{{ item.id }}. {{ item.title }}</p>
     </div>
   </v-main>
 </template>
 
 <script>
-export default {
-  data() {
-    return {
-      items: []
-    };
-  },
-  created() {
-    this.fetchData();
-  },
-  methods: {
-    async fetchData() {
-      try {
-        const response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
+  export default {
+    data() {
+      return {
+        items: []
+      };
+    },
+    created() {
+      this.fetchData();
+    },
+    methods: {
+      async fetchData() {
+        try {
+          const response = await fetch('https://jsonplaceholder.typicode.com/todos');
+          if (!response.ok) {
+            throw new Error('Network response was not ok');
+          }
+          const data = await response.json(); //parse data into json format
+          this.items = data; //data = what you bring from API server with fetch API
+        } catch (error) {
+          console.error('There was a problem with the fetch operation:', error);
         }
-        const data = await response.json();
-        this.items = data; // Assuming the response is an array of items
-      } catch (error) {
-        console.error('There was a problem with the fetch operation:', error);
       }
     }
   }
-}
 </script>
